94b1fbc7ca37855fc02919309618be1a3c4003c3
ViniRech <viniciusrech2007@gmail.com>
2025-06-13 19:15:57 -0300

    style: estilização da página de login #3

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,125 +3,95 @@
 </script>
 
 <template>
-    <main class="box-login-page">
-        <section class="login-section">
-            <h2>Campo de Login</h2>
-            <div>
-                <label for="user-name">Nome de usuário:</label>
-                <input type="text" name="user-name" id="user-name">
-            </div>
-            <div>
-                <label for="user-password">Senha:</label>
-                <input type="password" name="user-password" id="user-password">
-            </div>
-            <button>ENTRAR</button>
-            <div>
-                <p><a>Esqueceu a senha?</a></p>
-                <p><router-link to="/signup" exact>Fazer o cadastro</router-link></p>
-            </div>
-        </section>
-    </main>
+  <main class="box-login-page">
+    <section class="login-section">
+      <h2>Campo de Login</h2>
+      <div>
+          <label for="user-name">Nome de usuário:</label>
+          <input type="text" name="user-name" id="user-name">
+      </div>
+
+      <div>
+        <label for="user-password">Senha:</label>
+        <input type="password" name="user-password" id="user-password">
+      </div>
+
+      <button>ENTRAR</button>
+
+      <div>
+        <p><a>Esqueceu a senha?</a></p>
+        <p><a>Fazer o cadastro</a></p>
+      </div>
+    </section>
+  </main>
 </template>
 
 <style scoped>
  .box-login-page {
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        padding: 5rem 2rem;
-        justify-items: center;
-        align-items: center;
-      
-    }
+    display: grid;
+    padding: 5rem 2rem;
+    justify-items: center;
+    align-items: center;
+  }
 
-    .login-section {
-        display: grid;
-        justify-content: center;
-        align-content: center;
-        row-gap: 2rem;
-        color: var(--vt-c-white);
-        background-color: gray;
-        border-radius: 15px;
-        padding: 3rem;
-        border: 2px solid black;
-    }
+  .login-section {
+    display: grid;
+    justify-content: center;
+    align-content: center;
+    font-family: var(--fonte-principal);
+    row-gap: 2rem;
+    color: #000;
+    background-color: #f2f2f2;
+    border-radius: 5px;
+    padding: 3rem;
+    border: 1px solid black;
+  }
 
-    .login-section h2, .login-section div p {
-        text-align: center;
-    }
-       .login-section div {
-        display: grid;
-        grid-template-rows: auto auto;
-        row-gap: .5rem;
-    }
+  .login-section h2, .login-section div p {
+    text-align: center;
+  }
 
-    .login-section div input {
-        padding: .5rem;
-        border: 1px solid var(--vt-c-white);
-        background-color: var(--vt-c-transparent);
-        color: var(--vt-c-white);
-        border-radius: 6px;
-        width: 20vw;
-    }
+  .login-section div {
+    display: grid;
+    grid-template-rows: auto auto;
+    row-gap: .5rem;
+  }
 
-    .login-section div input:focus {
-        outline: 0;
-        border: 2px solid;
-    }
+  .login-section div input {
+    padding: .5rem;
+    border: 1px solid #000;
+    background-color: var(--vt-c-transparent);
+    color: #000;
+    border-radius: 6px;
+    width: 20vw;
+  }
 
-    .login-section button {
-        padding: .5rem;
-        border: none;
-        border-radius: 6px;
-        font-size: 1rem;
-        background-color: var(--vt-c-white);
-        color: var(--vt-c-black);
-    }
+  .login-section div input:focus {
+    outline: 0;
+    border: 2px solid;
+  }
 
-    .login-section button:hover {
-        background-color: var(--vt-c-light-blue);
-        color: var(--vt-c-white);
-    }
-    
-    .login-section button:active {
-        background-color: var(--vt-c-black);
-        color: var(--vt-c-white);
-    }
+  .login-section button {
+    padding: .5rem;
+    border: none;
+    border-radius: 6px;
+    font-size: 1rem;
+    background-color: #00000098;
+    color: #fff;
+  }
 
-    .login-section div p a {
-        color: var(--vt-c-white);
-    }
+  .login-section button:hover {
+    background-color: #000;
+    color: #fff;
+    cursor: pointer;
+  }
 
-    @media (max-width: 750px) {
-        .box-login-page {
-            grid-template-columns: auto;
-        }
+  .login-section div p a {
+    color: #000;
+  }
 
-        .box-login-page img {
-            display: none;
-        }
-
-        .login-section {
-            background-color: black;
-            color: var(--vt-c-dark-blue);
-            padding: 0;
-        }
-
-        .login-section div input {
-            border-color: #000;
-            width: auto;
-        }
-
-        .login-section div input:focus {
-            border-color: var(--vt-c-dark-blue);
-        }
-
-        .login-section button {
-            background-color: var(--vt-c-light-blue);
-            color: var(--v--vt-c-dark-blue);
-        }
-
-        .login-section div p a {
-            color: var(--vt-c-dark-blue);
-        }
-    }
+  .login-section div p a:hover {
+    cursor: pointer;
+    text-decoration: underline;
+  }
 </style>
