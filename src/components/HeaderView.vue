d3b39819564960e093214aac1461b61dcceeea33
ViniRech <viniciusrech2007@gmail.com>
2025-06-14 10:57:36 -0300

    feat: conclusão do cabeçalho #8

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -1,5 +1,47 @@
 <template>
-  <h1>Cabeçalho</h1>
-  <RouterLink to="/">Home</RouterLink> <br>
-  <RouterLink to="/login">Login</RouterLink> <br>
+  <header>
+    <div class="cabecalho-titulo">
+      <h1><RouterLink to="/">Belíssima</RouterLink></h1>
+    </div>
+
+    <div class="cabecalho-links">
+      <p><RouterLink to="/login">entrar</RouterLink></p>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+        <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
+      </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
+        <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
+      </svg>
+    </div>
+  </header>
 </template>
+
+<style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 1.5rem;
+    border-bottom: 1px solid var(--cor-cinza-auxiliar);
+    font-family: var(--fonte-principal);
+  }
+
+  .cabecalho-titulo h1 a {
+    color: black;
+    text-decoration: none;
+    font-weight: 100;
+    font-style: italic;
+  }
+
+  .cabecalho-links {
+    display: flex;
+    column-gap: 2rem;
+    align-items: center;
+
+    & p a {
+      color: black;
+      text-decoration: none;
+      font-weight: 300;
+      font-size: .8rem;
+    }
+  }
+</style>
