9f91af26c315e326a13c3a55805b5919e661dda5
ReehLimas <reehlimasil08@gmail.com>
2025-06-26 11:52:09 -0300

    feat: criação da página de vendas #13

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,11 @@ const router = createRouter({
       name: 'login',
       component: () => import('../views/LoginView.vue'),
     },
+    {
+      path: '/vendas',
+      name: 'vendas',
+      component: () => import('../views/VendaView.vue'),
+    },
   ],
 })
 
---
+++ src/views/VendaView.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref} from 'vue'
+
+    const dias = ref([
+  {id: 1, nome: "Domingo", alt: ""},
+  {id: 2, nome: "Segunda", alt: ""},
+  {id: 3, nome: "Terça", alt: ""},
+  {id: 4, nome: "Quarta", alt: ""},
+  {id: 5, nome: "Quinta", alt: ""},
+  {id: 6, nome: "Sexta", alt: ""},
+  {id: 7, nome: "Sábado", alt: ""},
+ 
+])
+</script>
+
+<template>
+    
+    <section class="header-dias">
+        <table> 
+      <tr> 
+        <th v-for = "dia in dias" :key="dia.id" class = "item-cabeçalho">
+           {{ dia.nome }} 
+        </th>
+        </tr> 
+        </table>     
+    </section>
+</template>
+<style scoped>
+.header-dias {
+    display: flex;
+    gap: 1.5rem;
+    width: 100%;
+    margin-top: 3rem;
+    padding: 1rem;
+    justify-content: center;
+  }
+.header-dias table{
+    margin: 2rem;
+}
+</style>
