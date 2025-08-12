ba501a0af059b481f510e5b8bde6e09b919ac2f7
fernandapadilha <vitoriafernandaifc@gmail.com>
2025-08-12 16:49:07 -0300

    feat: criação da página de perfil do usuário #14

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,35 @@
 <script setup>
-import HeaderView from './components/HeaderView.vue'
-import FooterView from './components/FooterView.vue'
+
 </script>
 
 <template>
-  
-  <HeaderView />
-  <RouterView />
-  <FooterView />
-
+   <h1>Meu perfil</h1>
+   <div>
+    <p>Nome completo:</p>
+    <p>Apelido:</p>
+  </div>
+  <div>
+    <h2>Preferências</h2>
+    <p>Quais são suas categorias favoritas?</p>
+    <ul>
+      <li>curto</li>
+      <li>midi</li>
+      <li>longo</li>
+      <li>noiva</li>
+      <li>festa</li>
+    </ul>
+  </div>
+  <div>
+    <h2>Minhas medidas</h2>
+    <p>Minhas Medições ajuda a simplificar o processo de encontrar a sua adaptação perfeita!</p>
+    <ul>
+      <li>altura:</li>
+      <li>peso</li>
+      <li>busto:</li>
+      <li>cintura:</li>
+      <li>quadril:</li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
