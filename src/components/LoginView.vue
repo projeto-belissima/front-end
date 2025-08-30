31c6a1e6ff57f5337af5caa92daf8cb42367cbcb
ViniRech <viniciusrech2007@gmail.com>
2025-08-30 14:17:18 -0300

    feat: componentização finalizada #24

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -6,12 +6,11 @@ const loginConfig = usarLoginConfig();
 <template>
   <main class="box-login-page">
     <section class="login-section">
-      <button @click="abrirFecharCampo(loginConfig.valorVF)" class="botao-saida">
+      <button @click="loginConfig.abrirFecharCampo(loginConfig.valorVF)" class="botao-saida">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
           <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
         </svg>
       </button>
-      {{ loginConfig.valorVF }}
 
       <h2>campo de login</h2>
       <div>
--- src/stores/login.js
+++ src/stores/login.js
@@ -4,8 +4,8 @@ import { defineStore } from 'pinia';
 export const usarLoginConfig = defineStore('login', () => {
   const valorVF = ref(false)
 
-  function abrirFecharCampo(boolean) {
-    boolean.value = !boolean.value
+  function abrirFecharCampo() {
+    valorVF.value = !valorVF.value
   }
   return { valorVF, abrirFecharCampo }
 })
