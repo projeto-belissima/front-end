62780fdd4e748144fe67587331a92659992534a9
antuneseds <esterdossantosantunes04@gmail.com>
2025-09-02 16:52:06 -0300

    Feat: aplicação dos botôes das cores #16

--- src/views/VestidosView.vue
+++ src/views/VestidosView.vue
@@ -23,9 +23,14 @@ const imgUrl = (img) => new URL(`../assets/img/${img}`, import.meta.url).href
       <h5 class="nome-produto">{{ vestido.nome }}</h5>
       <p class="descricao">{{ vestido.descricao }}</p>
       <h5 class="valor-produto">R$ {{ vestido.mediaPreco.replace('.', ',') }}</h5>
-      <h5 class="cores">{{ vestido.cores }}</h5>
-      <h5 class="medidas">{{ vestido.medidas }}</h5>
+      <h5 class="cores">Cor: {{ vestido.cores }}</h5>
+      <div class="selecao-cor">
+        <div class="button"><button class="azul botao-cor-selecionado"></button></div>
+        <div class="button"><button class="marrom"></button></div>
+        <div class="button"><button class="rosa"></button></div>
+      </div>
     </div>
+
   </section>
 </template>
 
@@ -67,5 +72,37 @@ const imgUrl = (img) => new URL(`../assets/img/${img}`, import.meta.url).href
   font-family: var(--fonte-principal);
   font-size: 1rem;
   font-weight: 300;
+}
+
+.selecao-cor {
+  display: flex;
+  gap: .5rem;
+
+  & div.button {
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    height: 25px;
+    width: 40px;
+    border: 1px solid gray;
+    border-radius: 15px;
+  }
+
+  & div > button {
+    height: 20px;
+    width: 35px;
+    border-radius: 15px;
+    border: none;
+  }
+}
+
+.azul {
+  background-color: blue;
+}
+.marrom {
+  background-color: brown;
+}
+.rosa {
+  background-color: palevioletred;
 }
 </style>
