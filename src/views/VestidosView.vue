3f770049f52b32e50a0366fc7bb236890b634b5f
fernandapadilha <vitoriafernandaifc@gmail.com>
2025-09-19 09:17:35 -0300

    feat: criação de carrossel para as fotos de vestido #33

--- src/views/VestidosView.vue
+++ src/views/VestidosView.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { onMounted, ref } from 'vue'
 import { useVestidoStore } from '@/stores/vestido'
+import Swiper from 'swiper/bundle';
+
+import 'swiper/css/bundle';
 
 const props = defineProps(['id']);
 const vestido = ref({
@@ -8,7 +11,10 @@ const vestido = ref({
   descricao: '',
   media_preco: '0',
   cores: [],
-  capa: []
+  capa: [{
+    url: '',
+    description: '',
+  }],
 });
 const vestidoStore = useVestidoStore();
 
@@ -19,6 +25,30 @@ function formataPreco(preco) {
 onMounted(async () => {
   await vestidoStore.buscarVestidos()
   vestido.value = vestidoStore.pegarVestidoPorId(props.id)
+
+  new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    grabCursor: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    }
+  });
+
 })
 </script>
 
@@ -27,7 +57,31 @@ onMounted(async () => {
 
   <section class="vestido" v-if="vestido">
 
-    <img :src="vestido.capa[0]?.url" />
+    <div class="swiper">
+
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" v-for="(imagem, index) in vestido?.capa" :key="index">
+          <img :src="imagem?.url" :alt="imagem?.description">
+        </div>
+      </div>
+
+      <div class="swiper-button-prev">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-arrow-left-short"
+          viewBox="0 0 16 16">
+          <path fill-rule="evenodd"
+            d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5" />
+        </svg>
+      </div>
+      <div class="swiper-button-next">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-arrow-right-short"
+          viewBox="0 0 16 16">
+          <path fill-rule="evenodd"
+            d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
+        </svg>
+      </div>
+
+    </div>
+
     <div class="informacoes">
       <h5 class="nome-produto">{{ vestido.descritivo }}</h5>
       <p class="descricao">{{ vestido.descricao }}</p>
@@ -58,6 +112,37 @@ onMounted(async () => {
   & div.informacoes {
     padding-right: 10rem;
   }
+}
+
+.swiper {
+  width: 100%;
+  height: auto;
+  display: grid;
+  column-gap: .5rem;
+  align-items: center;
+}
+
+.swiper-slide {
+  display: grid;
+  justify-content: center;
+
+  & img {
+    width: 25vw;
+  }
+}
+
+.swiper-button-prev,
+.swiper-button-next {
+  width: 25px;
+  height: 25px;
+  background-color: white;
+  border-radius: 50%;
+  color: black;
+}
+
+.swiper-button-prev::after,
+.swiper-button-next::after {
+  content: none;
 }
 
 .nome-produto {
