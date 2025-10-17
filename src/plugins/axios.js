97a82188ca853bb8a3b85980f5744b75df292e15
ViniRech <viniciusrech2007@gmail.com>
2025-10-17 11:01:54 -0300

    fix: correção de código #38

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Configura a baseURL usando a variável de ambiente
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL || 'https://belissima-deoi.onrender.com/api'
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL || 'http://localhost:19003/api'
 
 // axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('psg_auth_token')}`
 
