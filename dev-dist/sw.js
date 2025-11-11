1ba1e422d4fbf9be759edde123772cbbce575fa1
fernandapadilha <vitoriafernandaifc@gmail.com>
2025-11-11 16:07:05 -0300

    fix: link correto do banco

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mbl5glb6jvg"
+    "revision": "0.j0k8tb1dkm8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
